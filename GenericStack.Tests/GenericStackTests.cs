6b95e14f767f8bceaea538452976ec298153afba
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-06 13:10:15 +0200

    Add failing test for popping an item

--- GenericStack.Tests/GenericStackTests.cs
+++ GenericStack.Tests/GenericStackTests.cs
@@ -1,13 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GenericStack.Library;
 
 namespace GenericStack.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class GenericStacksTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanPopOffItem()
         {
+            var g_stack = new GenericStack<int>();
+            g_stack.Push(100);
+            Assert.AreEqual(100, g_stack.Pop());
         }
     }
 }
